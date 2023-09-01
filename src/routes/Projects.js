9cf6fee0cb2cf9d16dcee3a28844ac8659eb4662
Mariam Ali Omari <mamushkah001@gmail.com>
2023-09-01 16:14:23 +0300

    add service component, styling and responsiveness

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroSection from '../components/HeroSection'
+import Service from '../components/Service'
 
 const Projects = () => {
   return (
     <>
      <Navbar/>
     <HeroSection heading="MY PROJECTS" text= "Here are some of the projects I've had the privilege to work on:"/>
+    <Service/>
     <Footer/>
     </>
   )
