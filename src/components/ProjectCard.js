7519855aa858770c51f8284274e7e84b3126036a
Mariam Ali Omari <mamushkah001@gmail.com>
2023-09-03 09:00:28 +0300

    add projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import "./ProjectCardStyles.css"
+import portfolio1 from "../assets/portfolio.png"
+import { NavLink } from 'react-router-dom'
 
 const ProjectCard = () => {
   return (
-    <div>ProjectCard</div>
+    <div className='work-container'>
+      <h1 className='project-heading'>Projects</h1>
+      <div className='project-container'>
+        <div className='project-card'>
+          <img  src= {portfolio1} alt="img"></img>
+          <h2 className='project-title'> Project title</h2>
+          <div className='project-details'>
+            <p>this is my paragraph</p>
+            <div className='pro-btns'>
+              <NavLink to ="url.com" className ="btn"> View</NavLink>
+              <NavLink to ="url.com" className ="btn"> Source</NavLink>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroSection from '../components/HeroSection'
 import Service from '../components/Service'
+import ProjectCard from '../components/ProjectCard'
 
 const Projects = () => {
   return (
@@ -10,6 +11,7 @@ const Projects = () => {
      <Navbar/>
     <HeroSection heading="MY PROJECTS" text= "Here are some of the projects I've had the privilege to work on:"/>
     <Service/>
+    <ProjectCard/>
     <Footer/>
     </>
   )
