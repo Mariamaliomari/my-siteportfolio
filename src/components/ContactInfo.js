002b58967dbabe95859f053a31021d74ad6fc5cf
Mariam ali omari <mamushkah001@gmail.com>
2023-12-07 22:45:23 +0300

    fix bug on emalijs

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,34 +1,44 @@
-import "./ContactInfo.css"
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
-
-// import React from 'react'
+import "./ContactInfo.css";
 
 const ContactInfo = () => {
-
-
   const form = useRef();
+  const [isSuccess, setIsSuccess] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.sendForm('service_8e0mpc6', 'template_mx3j84j', form.current, 'qEifGzjy0HQvfZMDX')
       .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
+        console.log(result.text);
+        console.log("message sent");
+        setIsSuccess(true);
+        resetForm();
+      })
+      .catch((error) => {
+        console.log(error.text);
       });
   };
+
+  const resetForm = () => {
+    form.current.reset();
+  };
+
   return (
     <div className="form">
-        <form>
-            <label>Enter Name</label>
-            <input type="text" name="user_name"></input>
-            <label>Email</label>
-            <input type="email" name="user_email"></input>
-            <label>Message</label>
-            <textarea rows="6" placeholder="Type your message here" name="message"/>
-            <input
+      {isSuccess && <div className="success-message">Message sent successfully!</div>}
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Enter Name</label>
+        <input type="text" name="user_name" />
+
+        <label>Email</label>
+        <input type="email" name="user_email" />
+
+        <label>Message</label>
+        <textarea rows="6" placeholder="Type your message here" name="message" />
+
+        <input
           type="submit"
           value="Send"
           style={{
@@ -46,24 +56,151 @@ const ContactInfo = () => {
             e.target.style.color = "white";
           }}
         />
-
-            
-
-        </form>
-      
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default ContactInfo
+export default ContactInfo;
 
 
-// {/* <form ref={form} onSubmit={sendEmail}>
-//       <label>Name</label>
-//       <input type="text" name="user_name" />
-//       <label>Email</label>
-//       <input type="email" name="user_email" />
-//       <label>Message</label>
-//       <textarea name="message" />
-//       <input type="submit" value="Send" />
-//     </form> */}
+
+
+// 
+// import React, { useRef } from 'react';
+// import emailjs from '@emailjs/browser';
+
+// const ContactInfo = () => {
+//   const form = useRef();
+//   const [isSuccess, setIsSuccess] = useState(false);
+
+//   const sendEmail = (e) => {
+//     e.preventDefault();
+
+//     emailjs.sendForm('service_8e0mpc6', 'template_mx3j84j', form.current, 'qEifGzjy0HQvfZMDX')
+//       .then((result) => {
+//         console.log(result.text);
+//         console.log("message sent");
+//         setIsSuccess(true);
+//         resetForm();
+
+
+//       })
+//       .catch((error) => {
+//         console.log(error.text);
+
+//       })
+//       const resetForm = () => {
+//              form.current.reset();}
+
+      
+
+      
+//   };
+  
+
+//   return (
+//     <div className="form">
+//       {isSuccess && <div className="success-message">Message sent successfully!</div>}
+//       <form ref={form} onSubmit={sendEmail}>
+//         <label>Enter Name</label>
+//         <input type="text" name="user_name" />
+
+//         <label>Email</label>
+//         <input type="email" name="user_email" />
+
+//         <label>Message</label>
+//         <textarea rows="6" placeholder="Type your message here" name="message" />
+
+//         <input
+//           type="submit"
+//           value="Send"
+//           style={{
+//             background: "rgb(65, 140, 238)",
+//             fontSize: "20px",
+//             color: "white",
+//             transition: "background 0.3s, color 0.3s",
+//           }}
+//           onMouseOver={(e) => {
+//             e.target.style.background = "white";
+//             e.target.style.color = "darkslategray";
+//           }}
+//           onMouseOut={(e) => {
+//             e.target.style.background = "rgb(65, 140, 238)";
+//             e.target.style.color = "white";
+//           }}
+//         />
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default ContactInfo;
+
+
+
+
+
+// import React, { useRef, useState } from 'react';
+// import emailjs from '@emailjs/browser';
+
+// const ContactInfo = () => {
+//   const form = useRef();
+//   const [isSuccess, setIsSuccess] = useState(false);
+
+//   const sendEmail = (e) => {
+//     e.preventDefault();
+
+//     emailjs.sendForm('service_8e0mpc6', 'template_mx3j84j', form.current, 'qEifGzjy0HQvfZMDX')
+//       .then((result) => {
+//         console.log(result.text);
+//         console.log("message sent");
+//         setIsSuccess(true);
+//         resetForm();
+//       })
+//       .catch((error) => {
+//         console.log(error.text);
+//       });
+//   };
+
+//   const resetForm = () => {
+//     form.current.reset();
+//   };
+
+//   return (
+//     <div className="form">
+//       {isSuccess && <div className="success-message">Message sent successfully!</div>}
+//       <form ref={form} onSubmit={sendEmail}>
+//         <label>Enter Name</label>
+//         <input type="text" name="user_name" />
+
+//         <label>Email</label>
+//         <input type="email" name="user_email" />
+
+//         <label>Message</label>
+//         <textarea rows="6" placeholder="Type your message here" name="message" />
+
+//         <input
+//           type="submit"
+//           value="Send"
+//           style={{
+//             background: "rgb(65, 140, 238)",
+//             fontSize: "20px",
+//             color: "white",
+//             transition: "background 0.3s, color 0.3s",
+//           }}
+//           onMouseOver={(e) => {
+//             e.target.style.background = "white";
+//             e.target.style.color = "darkslategray";
+//           }}
+//           onMouseOut={(e) => {
+//             e.target.style.background = "rgb(65, 140, 238)";
+//             e.target.style.color = "white";
+//           }}
+//         />
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default ContactInfo;
